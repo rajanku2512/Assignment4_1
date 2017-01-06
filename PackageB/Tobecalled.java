3616dccdd7ccd4d00af8da577ea35004f09e140e
rajanku2512 <rajanku2512@gmail.com>
2017-01-06 21:36:04 +0530

    Add files via upload

---
+++ PackageB/Tobecalled.java
@@ -0,0 +1,15 @@
+package PackageB;
+
+
+
+ public class Tobecalled {
+ protected void calling (String name, String emailid){
+		
+		System.out.println("rajan" );
+		System.out.println("xyz@gmail.com");
+		
+		
+	
+		
+		}
+}
---
+++ RAJ/Calling.java
@@ -0,0 +1,14 @@
+package RAJ;
+
+import PackageB.*;
+import java.util.*;
+
+public class Calling extends Tobecalled {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calling ram = new Calling();
+		ram.calling("name", "emailid");
+	}
+}
